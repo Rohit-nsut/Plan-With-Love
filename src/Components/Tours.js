59bc77c8fde2ef170733af127067b8f8284838b2
Roht <rohittondak38@gmail.com>
2024-12-06 21:15:20 +0530

    New Commit

--- src/Components/Tours.js
+++ src/Components/Tours.js
@@ -7,7 +7,7 @@ const Tours = (props) => {
     return (
         <div className="">
             <div className="flex flex-col justify-center items-center">
-                <div className="my-10 text-4xl sm:text-5xl md:text-6xl  font-semibold border-[0.3rem]  border-dashed  border-blue-900 sm:py-4 sm:px-20 rounded-2xl transition-all py-3 px-14 sm:font-bold sm:border-[0.4rem]">
+                <div className="my-10 text-3xl sm:text-5xl md:text-6xl  font-semibold border-[0.3rem]  border-dashed  border-blue-900 sm:py-4 sm:px-20 rounded-2xl transition-all py-3 px-12 sm:font-bold sm:border-[0.4rem]">
                     <h1 className="">Plan With Love</h1>
                 </div>
 
